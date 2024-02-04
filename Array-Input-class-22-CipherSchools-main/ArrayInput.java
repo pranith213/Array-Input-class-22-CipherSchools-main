7f97ab6f78e53dd9778907ab630c54e9b1f8b838
pranith213 <126413130+pranith213@users.noreply.github.com>
2024-02-04 22:33:04 +0530

    Add files via upload

---
+++ Array-Input-class-22-CipherSchools-main/ArrayInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArrayInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        // input the size
+        int n = sc.nextInt();
+        // create the array
+        int arr[] = new int[n];
+        // take the elements as input
+        for(int i = 0; i < n; i++){
+            System.out.println("Please enter " + (i+1) + " element ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Array is: ");
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
